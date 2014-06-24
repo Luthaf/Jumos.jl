abe62b149e61d63fce02d556a0c3cf27736e67ab
Guillaume Fraux <guillaume.fraux@ens.fr>
2014-06-24 17:25:32 +0100

    Adds firsts tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,24 @@
 using MolecularAnalysis
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+#===============================================================================
+                    Open and read a frame from the trajectories
+===============================================================================#
+
+const TEST_DIR = dirname(Base.source_path())
+
+# NetCDF
+traj = opentraj("$TEST_DIR/trjs/water.nc", topology="$TEST_DIR/trjs/water.lmp")
+
+frame = Frame(traj)
+read_frame!(traj, 500, frame)
+
+close(traj)
+
+# XYZ
+traj = opentraj("$TEST_DIR/trjs/water.xyz", box=[15.0, 15.0, 15.0])
+
+frame = Frame(traj)
+read_frame!(traj, 500, frame)
+
+close(traj)
