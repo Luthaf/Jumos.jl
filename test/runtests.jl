781babace1e118ac11866907e4b341c67165b78d
Guillaume Fraux <guillaume.fraux@ens.fr>
2014-10-29 15:52:01 +0100

    Refactor tests to get more flexibility

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,47 @@
-tic()
 using Jumos
-println("Load time: $(toq())")
 using Base.Test
 
-include("vect3d.jl")
+const tests_available = Dict(:vect3d => "3D vector class",
+                             :trajectories => "TrajectoriesIO capacities",
+                             :analysis => "Trajectories analysis"
+                             )
+const SEP = "   "
 
-include("trajectories.jl")
+function main()
+    tests_selected = Symbol[]
+    if length(ARGS) > 0
+        for arg in ARGS
+            if symbol(arg) in keys(tests_available)
+                push!(tests_selected, symbol(arg))
+            elseif (arg == "--help") || (arg == "-h")
+                usage()
+                exit()
+            else
+                warn("Option $arg not found")
+            end
+        end
+    else
+        # Run all tests as default
+        tests_selected = keys(tests_available)
+    end
 
-include("histograms.jl")
+    for test in tests_selected
+        runtest(test)
+    end
+end
+
+function runtest(test)
+    filename = string(test) * ".jl"
+    include(filename)
+end
+
+function usage()
+    println("Select the test you want to run:")
+    for (test, help) in tests_available
+        println(SEP * string(test) * SEP * help)
+    end
+    println("Options:")
+    println(SEP * "--help (-h): show this help")
+end
+
+main()
