87d056616f6f33ae0f54473e9086d2f528328fd6
Guillaume Fraux <guillaume.fraux@ens.fr>
2014-11-05 21:38:02 +0100

    [doc] Simple example script
    This will also be used as a test for the code. It do not run yet,
    so I guess this is TDD.

--- doc/simulations/usage.rst
+++ doc/simulations/usage.rst
@@ -1,2 +1,24 @@
 Usage example
 =============
+
+With `Jumos`, you run simulations by writting Julia scripts. You don't need to
+know much of Julia, and if you already have done some C++ or Python you should
+feel familiar. Just remember that array index start at 1, not 0. If you come from
+FORTRAN, the main point will be that you can access composit types fields using the
+the dot notation : ``foo.bar = 3`` will set the ``bar`` field of ``foo`` to 3.
+
+Lennard-Jones fluid
+-------------------
+
+Here is a simple simulation script for running a simulation of a Lennard-Jones
+fluid at :math:`300K`.
+
+.. literalinclude:: /../examples/lennard-jones.jl
+   :language: julia
+   :linenos:
+
+TODO : defaults values in simulation
+
+
+Overwrite the algorithm
+-----------------------
---
+++ examples/lennard-jones.jl
@@ -0,0 +1,32 @@
+using Jumos
+
+sim = Simulation("MD")
+
+# Create the simulation box : cubic simulation box with a width of 20A
+create_box(sim, 20.0)
+
+# Create the initial topology, positions and velocities
+read_topology(sim, "lennard-jones.xyz")
+read_positions(sim, "lennard-jones.xyz")
+create_velocities(sim, 300)  # Initialize at 300K
+
+# Add Lennard-Jones interactions between He atoms
+add_interaction(sim, LennardJones(0.3, 2.0), ("He", "He"))
+
+# Set timestep to 1.0 fs
+set_timestep(sim, 1.0)
+
+out_trajectory = TrajectryOutput("LJ-traj.xyz", 1)
+add_output(sim, out_trajectory)
+
+add_output(sim, EnergyOutput(sim, 10))
+
+run!(sim, 500)
+
+# It is really easy to change some parameters if you bind them to variables
+out_trajectory.frequency = 10
+
+run!(sim, 5000)
+
+# Simulation scripts are normal julia scripts !
+println("All done")
