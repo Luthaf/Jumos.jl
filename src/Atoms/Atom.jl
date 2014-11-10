ef2e462157665aebae475f419b09f37d066d86f8
Guillaume Fraux <guillaume.fraux@ens.fr>
2014-11-10 18:50:45 +0100

    Update periodic table information to use units

--- src/Atoms/Atom.jl
+++ src/Atoms/Atom.jl
@@ -8,10 +8,11 @@ export Atom
 type Atom
     name::String                # atom name
     symbol::String              # atom chemical type
-    special::Dict{String, Any}  # special values (mass, charge, ...)
+    mass::Float64               # atomic mass
+    special::Dict{String, Any}  # special values (charge, ...)
 end
 
-Atom(s::String) = Atom(s, s, Dict())
+Atom(s::String) = Atom(s, s, 0, Dict())
 Atom() = Atom("")
 
 function show(io::IO, atom::Atom)
