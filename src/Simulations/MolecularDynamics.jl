0d97c5c8109684759c7ebbee373c5463e62cd766
Guillaume Fraux <guillaume.fraux@ens.fr>
2014-11-19 15:39:59 +0100

    Fix the check_interaction method
    ref 4c136042d599310e5b78dc4056d2054a58845868

--- src/Simulations/MolecularDynamics.jl
+++ src/Simulations/MolecularDynamics.jl
@@ -113,25 +113,24 @@ function check_settings(sim::MDSimulation)
 end
 
 function check_interactions(sim::MDSimulation)
+    atomic_types = sim.topology.atom_types
+
     atomic_pairs = Set{(Integer, Integer)}()
-    for (i, j, potential) in sim.interactions
-        union!(atomic_pairs, (i, j))
-        if potential ∉ sim.potentials
-            warn("Adding the potential $(potential.potential) to the simulation")
-            push!(sim.potentials, potential)
-        end
+    for (i, j) in keys(sim.interactions)
+        union!(atomic_pairs, [(i, j)])
     end
 
-    all_atomic_pairs = IntSet()
-    ntypes = size(sim.topology)
+    all_atomic_pairs = Set{(Integer, Integer)}()
+    ntypes = size(atomic_types, 1)
     for i=1:ntypes, j=1:ntypes
         union!(all_atomic_pairs, [(i, j), (j, i)])
     end
     setdiff!(all_atomic_pairs, atomic_pairs)
-    if size(all_atomic_pairs) != 0
+    if length(all_atomic_pairs) != 0
         missings = ""
         for (i, j) in all_atomic_pairs
-            missings *= string(sim.atoms[i].name) * " - " * string(sim.atoms[j].name) * "\n"
+            missings *= (string(atomic_types[i].name) * " - " *
+                         string(atomic_types[j].name) * "\n")
         end
         throw(SimulationConfigurationError(
             "The following atom pairs do not have any interaction:
