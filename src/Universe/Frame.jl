ac365997d97eb61079fdc66dd1bac9db6f72b59e
Guillaume Fraux <guillaume.fraux@ens.fr>
2014-12-02 09:47:32 +0100

    Use 0-length Array3D instead of Union(Array3D, Void)
    This give me more type-stable functions

--- src/Universe/Frame.jl
+++ src/Universe/Frame.jl
@@ -5,33 +5,30 @@
 import Base: size
 export Frame, set_frame_size!
 
-# TODO: use Array3D with size of 0 instead ?
-NullableArray3D = Union(Void, Array3D)
-
 type Frame
     step::Integer
     box::SimBox
     topology::Topology
     positions::Array3D
-    velocities::NullableArray3D
+    velocities::Array3D
 end
 
 Frame(t::Topology) = Frame(0,
                            SimBox(),
                            t,
                            Array3D(Float32, size(t.atoms, 1)),
-                           nothing)
+                           Array3D(Float32, 0))
 
 # Empty frame construction
-Frame() = Frame(0,SimBox(), Topology(), Array3D(Float64, 0), nothing)
+Frame() = Frame(0,SimBox(), Topology(), Array3D(Float64, 0), Array3D(Float32, 0))
 
 size(f::Frame) = length(f.positions)
 
 
-function set_frame_size!(frame::Frame, wanted_size::Integer)
+function set_frame_size!(frame::Frame, wanted_size::Integer; velocities=false)
     if size(frame) != wanted_size
         frame.positions = Array3D(Float32, wanted_size)
-        if frame.velocities != nothing
+        if velocities || length(frame.velocities) != 0
             frame.velocities = Array3D(Float32, wanted_size)
         end
     end
