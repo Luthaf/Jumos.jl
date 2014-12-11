f8f57e612b1acde4a26f390affefe10cdb71a27f
Guillaume Fraux <guillaume.fraux@ens.fr>
2014-12-11 20:58:36 +0100

    Add EnergyCompute
    This compute and store potential, kinetic and total energy

--- src/Simulations/MD/compute.jl
+++ src/Simulations/MD/compute.jl
@@ -5,7 +5,7 @@ import Base: call
 using Jumos.Constants #kB
 
 export BaseCompute
-export TemperatureCompute, PressureCompute, VolumeCompute
+export TemperatureCompute, PressureCompute, VolumeCompute, EnergyCompute
 
 # abstract BaseCompute -> Defined in MolecularDynamics.jl
 
@@ -18,11 +18,8 @@ end
 
 function call(::TemperatureCompute, sim::MDSimulation)
 	T = 0.0
-	K = 0.0
-	natoms = size(sim.frame)
-	@inbounds for i=1:natoms
-		K += 0.5 * sim.masses[i] * dot(sim.frame.velocities[i], sim.frame.velocities[i])
-	end
+    K = kinetic_energy(sim)
+    natoms = size(sim.frame)
 	T = 1/kB * 2*K/(3*natoms)
 	sim.data[:temperature] = T
 	return T
@@ -34,4 +31,36 @@ end
 
 type VolumeCompute <: BaseCompute
 
+end
+
+type EnergyCompute <: BaseCompute end
+
+function call(::EnergyCompute, sim::MDSimulation)
+    K = kinetic_energy(sim)
+    P = potential_energy(sim)
+	sim.data[:E_kinetic] = K
+    sim.data[:E_potential] = P
+    sim.data[:E_total] = P + K
+	return K, P, P + K
+end
+
+function kinetic_energy(sim::MDSimulation)
+    K = 0.0
+	natoms = size(sim.frame)
+	@inbounds for i=1:natoms
+		K += 0.5 * sim.masses[i] * dot(sim.frame.velocities[i], sim.frame.velocities[i])
+	end
+    return K
+end
+
+function potential_energy(sim::MDSimulation)
+    E = 0.0
+	natoms = size(sim.frame)
+	@inbounds for i=1:natoms, j=(i+1):natoms
+        atom_i = sim.frame.topology.atoms[i]
+        atom_j = sim.frame.topology.atoms[j]
+        potential = sim.interactions[(atom_i, atom_j)]
+		E += potential(distance(sim.frame, i, j))
+	end
+    return E
 end
