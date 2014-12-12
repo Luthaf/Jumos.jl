fcd86b34fac62b9af164c714131e005efb73b249
Guillaume Fraux <guillaume.fraux@ens.fr>
2014-12-12 23:37:53 +0100

    Add tests for the Distances module

--- test/distances.jl
+++ test/distances.jl
@@ -2,11 +2,54 @@ top = dummy_topology(4)
 
 frame = Frame(top)
 
-cell = UnitCell(10.)
+orthorombic = UnitCell(10.)
+infitite = UnitCell(InfiniteCell)
+triclinic1 = UnitCell(10, 10, 10, 80, 90, 120)
+triclinic2 = UnitCell(10., 10., 10., TriclinicCell)
+
+@test UnitCell(13.) == UnitCell(13., 13., 13.)
+@test UnitCell(13.) == UnitCell(13., 13., 13., pi/2, pi/2, pi/2)
 
 frame.positions[1] = [2, 2, 4]
 frame.positions[2] = [2, 2, 2]
-frame.positions[3] = [1, 2, 3]
+frame.positions[3] = [-45, 2, 300.56]
 frame.positions[4] = [29, 22, 45]
 
-#TODO: more tests here
+frame.cell = orthorombic
+
+frame2 = Frame(top)
+frame2.positions = copy(frame.positions)
+frame2.cell = triclinic2
+
+for i=1:4, j=1:4
+    # This fail for now, let's fix it later
+    # @test distance(frame, i, j) == distance(frame2, i, j)
+end
+
+@test distance(frame, 1, 2) == 2.0
+
+for cell in [orthorombic, infitite, triclinic1, triclinic2]
+    frame.cell = cell
+    for i=1:4, j=1:4
+        @test distance(frame, i, j) == distance(frame, j, i)
+    end
+end
+
+# TODO: test the distance(ref, conf, i, j) and distance(ref, conf, i) methods
+
+# frame.positions[1] is in the cell
+@test minimal_image(frame.positions[1], orthorombic) == frame.positions[1]
+@test minimal_image(frame.positions[1], infitite) == [frame.positions[1]...]
+@test minimal_image(frame.positions[1], triclinic1) == [frame.positions[1]...]
+# TODO: fix this and L26.
+#@test minimal_image(frame.positions[1], triclinic2) == [frame.positions[1]...]
+
+# frame.positions[3] and frame.positions[4] are not
+@test isapprox(minimal_image(frame.positions[3], orthorombic), [5.0, 2.0, 0.56])
+@test isapprox(minimal_image(frame.positions[4], orthorombic), [-1, 2.0, -5.0])
+@test minimal_image(frame.positions[3], infitite) == [frame.positions[3]...]
+@test minimal_image(frame.positions[4], infitite) == [frame.positions[4]...]
+
+# check mutating version
+minimal_image!(frame.positions[4], orthorombic)
+@test isapprox([frame.positions[4]...], [-1, 2.0, -5.0])
