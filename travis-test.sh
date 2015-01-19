887c628143db6d1153f3be991649d2832a0ec062
Guillaume Fraux <guillaume.fraux@ens.fr>
2015-01-19 02:06:26 +0100

    Use FactCheck in Travis

--- travis-test.sh
+++ travis-test.sh
@@ -3,6 +3,7 @@ set -ev
 
 git fetch --unshallow
 julia -e 'Pkg.clone(pwd())'
+julia -e 'Pkg.add("FactCheck")'
 
 if [ -f test/runtests.jl ]; then
     julia --check-bounds=yes -e 'Pkg.test("Jumos", coverage=true)'
